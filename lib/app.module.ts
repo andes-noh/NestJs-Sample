e2817f319863cd37ed52cff3ac184b5a90543d97
andes-noh <nkj9693@gmail.com>
2022-08-21 00:13:12 +0900

    chore(app.module): scheduleModule 추가

--- lib/app.module.ts
+++ lib/app.module.ts
@@ -1,9 +1,10 @@
 import { TestModule } from './sample/test.module'
 import { Module } from '@nestjs/common'
+import { ScheduleModule } from '@nestjs/schedule'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [TestModule, ConfigModule.forRoot()],
+  imports: [TestModule, ScheduleModule.forRoot(), ConfigModule.forRoot()],
   providers: [],
   controllers: [],
 })
