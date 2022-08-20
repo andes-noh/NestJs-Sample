844c61826d297df07c42f459d046af2dc526f8b4
andes-noh <nkj9693@gmail.com>
2022-08-20 23:54:22 +0900

    fear(test.service): test service 추가

--- lib/app.module.ts
+++ lib/app.module.ts
@@ -1,11 +1,10 @@
 import { TestModule } from './sample/test.module'
-import { TestService } from './sample/test.service'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [TestModule, ConfigModule.forRoot()],
-  providers: [TestService],
+  providers: [],
   controllers: [],
 })
 export class AppModule {}
